175701593b838b101f2ef2e1e2db77ad23571778
dkati <dkatikaridis@gmail.com>
2022-01-16 00:38:04 +0200

    lib: DialogParentView: Clean up

--- DialogWizardLib/src/main/java/com/dialog/dialogwizardlib/core/DialogParentView.java
+++ DialogWizardLib/src/main/java/com/dialog/dialogwizardlib/core/DialogParentView.java
@@ -1,34 +1,23 @@
 package com.dialog.dialogwizardlib.core;
 
-import android.app.Dialog;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
-import com.dialog.dialogwizardlib.R;
-import com.dialog.dialogwizardlib.Utils;
 import com.dialog.dialogwizardlib.databinding.DialogwizardlibParentBinding;
 
 
 public class DialogParentView extends DialogFragment {
 
-    private DialogwizardlibParentBinding __binder;
-    private WizardContainer mContainer;
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        __binder = DialogwizardlibParentBinding.inflate(inflater);
+        com.dialog.dialogwizardlib.databinding.DialogwizardlibParentBinding __binder = DialogwizardlibParentBinding.inflate(inflater);
         return __binder.getRoot();
     }
 
@@ -38,7 +27,7 @@ public class DialogParentView extends DialogFragment {
 
         // this second container is needed in order to NOT have empty background while
         // switching fragments
-        mContainer = new WizardContainer(this);
+        WizardContainer mContainer = new WizardContainer(this);
         mContainer.show(requireActivity().getSupportFragmentManager(),"dialogwizardlib_container");
 
     }
